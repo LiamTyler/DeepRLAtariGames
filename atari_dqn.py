d8b7ab08ede8741608108f4a29e7ab026b222226
Liam Tyler <tyler.liam7@gmail.com>
2019-05-08 11:55:46 -0500

    fix: the sampling from the uniform buffer was incorrect. Also added huber loss, HE initialization, using the life lost instead of termination, and switched back to Adam optimizer all from recommendations

--- atari_dqn.py
+++ atari_dqn.py
@@ -2,6 +2,7 @@ import keras
 from keras.models import Sequential
 from keras.layers import *
 from keras.optimizers import *
+from keras.initializers import VarianceScaling
 import random
 import numpy as np
 import math
@@ -10,6 +11,13 @@ from collections import deque
 import matplotlib.pyplot as plt
 import time
 
+from keras.backend.tensorflow_backend import set_session
+import tensorflow as tf
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
+config.log_device_placement = True  # to log device placement (on which device the operation ran)
+sess = tf.Session(config=config)
+set_session(sess)  # set this TensorFlow session as the default session for Keras
 
 ATARI_SHAPE = (4, 105, 80)
 FRAMES_SKIPPED = 4
@@ -57,6 +65,9 @@ class UniformReplayBuffer:
     def size(self):
         return len(self.memory)
 
+    def __len__(self):
+        return self.size()
+
     # store as (state, action, reward, done)
     def add(self, state, action, reward, newState, done):
         L = len(self.memory)
@@ -72,20 +83,32 @@ class UniformReplayBuffer:
         return self.size() == self.capacity
 
     def sample(self, size):
-        samples = random.sample(range(FRAMES_SKIPPED, len(self.memory)), size)
+        samples = random.sample(range(FRAMES_SKIPPED, len(self.memory) - 1), size)
         batch = []
         for i in range(size):
             frame = samples[i]
             states = []
             for f in range(frame - 3, frame + 1):
-                states.append(self.memory[frame][0])
+                states.append(self.memory[f][0])
             newStates = []
             for f in range(frame - 2, frame + 2):
-                newStates.append(self.memory[frame][0])
+                newStates.append(self.memory[f][0])
             _, a, r, d = self.memory[frame]
             batch.append((states, a, r, newStates, d))
 
         return batch
+
+HUBER_LOSS_DELTA = 1.0
+def huber_loss(y_true, y_pred):
+    err = y_true - y_pred
+
+    cond = K.abs(err) < HUBER_LOSS_DELTA
+    L2 = 0.5 * K.square(err)
+    L1 = HUBER_LOSS_DELTA * (K.abs(err) - 0.5 * HUBER_LOSS_DELTA)
+
+    loss = tf.where(cond, L2, L1)
+
+    return K.mean(loss)
 
 class Network:
     def __init__(self, learningRate, inputShape, outputSize):
@@ -96,13 +119,14 @@ class Network:
 
     def buildModel(self):
         model = Sequential()
-        model.add(Lambda(lambda x: x / 255.0, output_shape=self.inputShape))
-        model.add(Conv2D(16, (8, 8), strides=(4,4), activation='relu', input_shape=(self.inputShape), data_format='channels_first'))
-        model.add(Conv2D(32, (4, 4), strides=(2,2), activation='relu'))
+        #model.add(Lambda(lambda x: x / 255.0, output_shape=self.inputShape))
+        model.add(Conv2D(16, (8, 8), strides=(4,4), activation='relu', input_shape=(self.inputShape), data_format='channels_first',
+                         kernel_initializer=VarianceScaling(scale=2.0)))
+        model.add(Conv2D(32, (4, 4), strides=(2,2), activation='relu', kernel_initializer=VarianceScaling(scale=2.0)))
         model.add(Flatten())
-        model.add(Dense(units=256, activation='relu'))
-        model.add(Dense(units=self.outputSize, activation='linear'))
-        model.compile(loss='mse', optimizer=RMSprop(lr=self.learningRate, rho=0.95, epsilon=0.01))
+        model.add(Dense(units=256, activation='relu', kernel_initializer=VarianceScaling(scale=2.0)))
+        model.add(Dense(units=self.outputSize, activation='linear', kernel_initializer=VarianceScaling(scale=2.0)))
+        model.compile(loss=huber_loss, optimizer=Adam(lr=self.learningRate))
         return model
 
     def predict(self, xs):
@@ -125,7 +149,7 @@ class Network:
         self.model = keras.models.load_model(path)
 
 class Agent:
-    def __init__(self, currentNet, buffer, gamma = 0.99, minEpsilon = 0.1, explorationFrames = 1000000, batchSize = 32):
+    def __init__(self, currentNet, tNetwork, buffer, gamma = 0.99, minEpsilon = 0.1, explorationFrames = 1000000, batchSize = 32, tau = 10000):
         self.replayBuffer = buffer
         self.GAMMA = gamma
         self.MIN_EPSILON = minEpsilon
@@ -133,6 +157,9 @@ class Agent:
         self.BATCH_SIZE = batchSize
 
         self.currentNetwork = currentNet
+        self.targetNetwork = tNetwork
+        self.updateTargetModel()
+        self.TAU = tau
         self.steps = 0
         self.epsilon = 1
 
@@ -142,25 +169,29 @@ class Agent:
         else:
             return np.argmax(self.currentNetwork.predictOne(state))
 
+    def updateTargetModel(self):
+        self.targetNetwork.setWeights(self.currentNetwork)
+
     def observe(self, state, action, reward, newState, done):
         self.replayBuffer.add(state, action, reward, newState, done)
         self.steps += 1
         if self.steps <= self.EXPLORATION_FRAMES:
             self.epsilon = self.MIN_EPSILON + (1.0 - self.MIN_EPSILON) * (self.EXPLORATION_FRAMES - self.steps) / self.EXPLORATION_FRAMES
 
+        if self.steps % self.TAU == 0:
+            self.updateTargetModel()
+
     def experienceReplay(self):
         # sample random batch from replay memory
-        if self.BATCH_SIZE + 4 > self.replayBuffer.size():
-            return
         batch = self.replayBuffer.sample(self.BATCH_SIZE)
 
         # combine all of the BATCH_SIZE states and new states into two lists
         # to let keras do the predictions in two calls
-        states = np.array([x[0] for x in batch])
-        newStates = np.array([x[3] for x in batch])
+        states = np.array([x[0] for x in batch]).astype(np.float32) / 255.0
+        newStates = np.array([x[3] for x in batch]).astype(np.float32) / 255.0
         
         currentPredictions = self.currentNetwork.predict(states)
-        newPredictions = self.currentNetwork.predict(newStates)
+        newPredictions = self.targetNetwork.predict(newStates)
         
         # Create list of xs and ys do train all of the batch items at once
         s = self.currentNetwork.inputShape
@@ -198,7 +229,7 @@ class RandomAgent:
     def experienceReplay(self):
         pass
 
-
+stateStack = None
 class Environment:
     def __init__(self, name):
         self.name = name
@@ -240,40 +271,63 @@ class Environment:
 
     def stepEnvironment(self, action):
         newState, reward, done, info = self.env.step(action)
+        reward = np.clip(reward, -1, 1)
         newState = self.processState(newState)
         return newState, reward, done, info
 
-    def run(self, agent, numEpisodes, verbose = True, render = False):
+    def run(self, agent, numSteps, verbose = True, render = False):
+        global stateStack
         self.agent = agent
-        for episode in range(numEpisodes):
-            state = self.processState(self.env.reset())
-            score = 0            
+        episode = 0
+        step = 0
+        while step < numSteps:
+            state = self.env.reset()
+            for _ in range(random.randint(1, 30)):
+                state , _, _, _ = self.env.step(0)
+            score, lives = 0, 5
+            state = self.processState(state)
             done = False
-            stateStack = np.array([state, state, state, state])
+            dead = False
+            stateStack = np.array([state, state, state, state]).astype(np.float32) / 255.0
             
             while not done:
                 if render:
                     self.env.render()
+                    time.sleep(.3)
                 
                 # select action
                 action = agent.selectAction(stateStack)
 
                 # execute that action
                 newState, reward, done, info = self.stepEnvironment(action)
+                if lives > info['ale.lives']:
+                    dead = True
+                    lives = info['ale.lives']
                 
                 # store experience in replay memory
-                agent.observe(state, action, reward, newState, done)
+                agent.observe(state, action, reward, newState, dead) # use dead, not done for breakout
                 agent.experienceReplay()
 
                 # observe reward and next state
                 score += reward
                 state = newState
+                s = newState.astype(np.float32) / 255.0
                 s = self.stateSpaceShape
                 stateStack = np.append(stateStack[1:], state.reshape((1, s[1], s[2])), axis=0)
 
+                if dead:
+                    dead = False
+                step += 1
+
+            episode += 1
             self.scores.append(score)
             if verbose:
-                print("Episode", episode, ":", score)
+                print("Episode:", episode, ", step:", agent.steps, ":", score)
+                if episode % 100 == 0:
+                    print("Last 100 episode average:", sum(self.scores[-100:]) / 100)
+                if agent.steps % 1000:
+                    agent.currentNetwork.save("breakout_cnet.h5")
+                    agent.targetNetwork.save("breakout_tnet.h5")
 
 def run(env):
     for episode in range(10):
@@ -294,17 +348,18 @@ def run(env):
             state = newState
             
 env = Environment('BreakoutDeterministic-v4')
-replayBuffer = UniformReplayBuffer(1000000)
+replayBuffer = UniformReplayBuffer(600000)
 
 randomAgent = RandomAgent(replayBuffer, env.actionSpaceSize)
-REPLAY_BUFFER_START_SIZE = 5000
+REPLAY_BUFFER_START_SIZE = 50000
 while replayBuffer.size() < REPLAY_BUFFER_START_SIZE:
-    env.run(randomAgent, 1, False, False)
+    env.run(randomAgent, 1000, False, False)
     print(replayBuffer.size() / REPLAY_BUFFER_START_SIZE)
-print("full")
+print("Replay Buffer Initialized")
 
-cNetwork = Network(0.00025, env.stateSpaceShape, env.actionSpaceSize)
-agent = Agent(cNetwork, replayBuffer)
+cNetwork = Network(0.00001, env.stateSpaceShape, env.actionSpaceSize)
+tNetwork = Network(0.00001, env.stateSpaceShape, env.actionSpaceSize)
+agent = Agent(cNetwork, tNetwork, replayBuffer)
 
-env.run(agent, 10000)
+env.run(agent, 10000000)
 env.fullSave('breakout_dqn')
